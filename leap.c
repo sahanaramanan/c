52c1ba2e6eb8284386a00544e9bf1fa13ad05e8c
sahanaramanan <47449689+sahanaramanan@users.noreply.github.com>
2019-02-08 18:49:33 +0530

    Update and rename grt.c to leap.c

---
+++ leap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a year");
+    scanf("%d",&n);
+    if(n%4==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+}
